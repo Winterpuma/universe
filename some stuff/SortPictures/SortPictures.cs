8f00c04be7bb28f22a5e3f7cf0584751515403b9
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-09-03 00:52:30 +0300

    Easy life with picture sorting machine

---
+++ some stuff/SortPictures/SortPictures.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        /// <summary>
+        /// Will get files from sourse and move them to dest\\YYYY\\MMDD, where
+        /// YYYYMMDD is part of filename.
+        /// </summary>
+        /// <param name="sourse"></param>
+        /// <param name="dest"></param>
+        /// <param name="spaceBeforeDate">"Img_20190903" space is 4</param>
+        static void MovePics(DirectoryInfo sourse, DirectoryInfo dest, int spaceBeforeDate = 0)
+        {
+            foreach (FileInfo file in sourse.GetFiles())
+            {
+                string year = file.Name.Substring(spaceBeforeDate, 4);
+                string date = file.Name.Substring(spaceBeforeDate + 4, 4);
+
+                string resDerictory = dest + "\\" + year + "\\" + date;
+                Directory.CreateDirectory(resDerictory);
+                file.CopyTo(resDerictory + "\\" + file.Name.Substring(spaceBeforeDate)); // will delete all chars before date in filename
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo sourse = new DirectoryInfo(@"D:\test");
+            DirectoryInfo dest = new DirectoryInfo(@"D:\test");
+
+            MovePics(sourse, dest, 0);
+        }
+    }
+}
